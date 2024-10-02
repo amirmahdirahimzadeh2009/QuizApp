5149c20c4ef4ca2f2e26be1b1f6c6f3e5594edff
amirmahdirahimzadeh2009 <158206164+amirmahdirahimzadeh2009@users.noreply.github.com>
2024-10-02 15:24:40 +0330

    feat

---
+++ src/js/quizData.js
@@ -0,0 +1,42 @@
+export const quizData = [
+  {
+    question: "What is the capital of France?",
+    a: "Berlin",
+    b: "Madrid",
+    c: "Paris",
+    d: "Rome",
+    correct: "c",
+  },
+  {
+    question: "Which language is used for web development?",
+    a: "Python",
+    b: "JavaScript",
+    c: "C++",
+    d: "Java",
+    correct: "b",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    a: "Earth",
+    b: "Mars",
+    c: "Jupiter",
+    d: "Saturn",
+    correct: "b",
+  },
+  {
+    question: "Who wrote 'Harry Potter'?",
+    a: "J.R.R. Tolkien",
+    b: "George R.R. Martin",
+    c: "J.K. Rowling",
+    d: "Suzanne Collins",
+    correct: "c",
+  },
+  {
+    question: "What is the square root of 64?",
+    a: "6",
+    b: "7",
+    c: "8",
+    d: "9",
+    correct: "c",
+  },
+];
